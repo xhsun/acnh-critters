a0daf71e45c2ecd958449c81e3d9f2a39b43ce92
xhsun <xhsun@users.noreply.github.com>
2020-05-05 21:34:13 -0400

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FISH_URL, BUG_URL } from './core/injection-tokens.store';
+import { FishTableComponent } from './fish-table/fish-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MonthStringPipe } from './core/month-string/month-string.pipe';
+import { LoggerModule, NgxLoggerLevel } from 'ngx-logger';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, FishTableComponent, MonthStringPipe],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatSortModule,
+    HttpClientModule,
+    LoggerModule.forRoot({
+      level: environment.production ? NgxLoggerLevel.ERROR : NgxLoggerLevel.DEBUG,
+    }),
+  ],
   providers: [
     { provide: FISH_URL, useValue: 'https://www.xhsun.me/acnh-api/data/v1/fish.json' },
     { provide: BUG_URL, useValue: 'https://www.xhsun.me/acnh-api/data/v1/bug.json' },
